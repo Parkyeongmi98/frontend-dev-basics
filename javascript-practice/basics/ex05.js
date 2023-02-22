6ecdb227e7098eb046375a9ecac82106e62949cb
Parkyeongmi98 <yeongmi_@naver.com>
2023-02-22 14:22:58 +0900

    js object #1

--- javascript-practice/basics/ex05.js
+++ javascript-practice/basics/ex05.js
@@ -1,19 +1,19 @@
 /*
-구문(statement)
-1. 자바스크립터 실행단위
-2. 구성 요소
-    값, 연산식: 표현식
-    주석: 구문이지만 실행하지 않는다.
-    예약어: if, else, for
-3. 구문의 예
-    if, if~else
-    while, do~while, for
-    for~in
-    switch
-    변수 정의
-4. 공백: 토큰 구분, 무시
-5. 세미콜론
-6. 개행(line breaker)
+    구문(statement)
+    1. 자바스크립터 실행단위
+    2. 구성 요소
+        값, 연산식: 표현식
+        주석: 구문이지만 실행하지 않는다.
+        예약어: if, else, for
+    3. 구문의 예
+        if, if~else
+        while, do~while, for
+        for~in
+        switch
+        변수 정의
+    4. 공백: 토큰 구분, 무시
+    5. 세미콜론
+    6. 개행(line breaker)
 */
 
 // ;(세미콜론)하는 역할은 표현식을 표현식구문으로 만들어 바로 실행하게끔 한다.
