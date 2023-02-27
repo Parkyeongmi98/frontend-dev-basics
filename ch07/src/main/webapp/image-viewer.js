77cf5f8c5ecce99fdde13292f2d2b7527b74293d
Parkyeongmi98 <yeongmi_@naver.com>
2023-02-27 18:46:36 +0900

    image-viewer #finish

--- ch07/src/main/webapp/image-viewer.js
+++ ch07/src/main/webapp/image-viewer.js
@@ -1,13 +1,34 @@
 imageViewer = {
 	init: function() {
-		for(var i = 0; i < 100; i++) {
-			this._changeImage();
-		}
+		$(imageViewer._initialize);
+	},
+	_initialize: function() {
+		$('#btn-change').click(imageViewer._changeImage);
+		$('#btn-slideshow').click(imageViewer._slideShow);
 	},
 	_changeImage: function() {
-		var index = Math.floor(Math.random()*this._images.length);
-		console.log(index);
+		var index = Math.floor(Math.random()*imageViewer._images.length);
+
+		//while($("img").attr("title")===imageViewer._images[index].name) {
+		//	index = Math.floor(Math.random()*imageViewer._images.length);
+		//}
+		
+		$("img").attr({
+			src: "images/" + imageViewer._images[index].file,
+			title: imageViewer._images[index].name
+		});
 	},
+	_slideShow: function() {
+		if(imageViewer._intervalId == null) { // intervalId가 null이면 실행
+			imageViewer._intervalId = setInterval(imageViewer._changeImage, 1000);
+			$('#btn-slideshow').text("슬라이드쇼 끝");
+		} else {
+			clearInterval(imageViewer._intervalId); // 멈추기
+			imageViewer._intervalId = null; // intervalId를 null로 만들어야 다시 시작할 수 있음
+			$('#btn-slideshow').text("슬라이드쇼 시작");
+		}
+	},
+	_intervalId : null,
 	_images: [{
 		name: '국화', 
 		file: 'Chrysanthemum.jpg'
